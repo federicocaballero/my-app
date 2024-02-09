e6b5d607931bb7b53a205db7f0a387b6988b5997
Federico Caballero <fedecaballero1234@gmail.com>
2024-02-08 22:49:16 -0300

    Creado y renderizado componente Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+'use client'
+import React, { Component } from 'react'
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <>
+        <nav class="bg-gray-800">
+            
+        </nav>
+
+      </>
+    )
+  }
+}
