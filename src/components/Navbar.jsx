d9b916277a33a62e0fd5911864387134a4b06bc9
Federico Caballero <fedecaballero1234@gmail.com>
2024-02-12 22:40:56 -0300

    Instalacion de Bootstrap para componente Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,11 @@ export default class Navbar extends Component {
   render() {
     return (
       <>
-        <nav class="bg-gray-800">
-            
-        </nav>
+        <nav className="navbar bg-white">
+        <div className='p-3'>
+            <img className='m-auto' src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/f5003255058795.597b831f0c8c5.png" alt="Logo" width="150" height="75" class="d-inline-block align-text-top"/>
+        </div>
+      </nav>
 
       </>
     )
